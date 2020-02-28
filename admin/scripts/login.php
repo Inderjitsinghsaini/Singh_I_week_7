720308c8776f283f214ea3bf9d555f31d5f5f629
Singh <i_singh94252@fanshawec.ca>
2020-02-27 19:31:50 -0500

    added files

---
+++ admin/scripts/login.php
@@ -0,0 +1,67 @@
+<?php
+function login($username, $password, $ip){
+    // return sprintf('You are trying username=>%s, password=>%s', $username, $password);
+
+    $pdo = Database::getInstance()->getConnection();
+    // Check existence of the user
+    $check_exist_query = 'SELECT COUNT(*) FROM tbl_user WHERE user_name =:username';
+    $user_set = $pdo->prepare($check_exist_query);
+    $user_set->execute(
+        array(
+            ':username'=>$username
+        )
+    );
+
+    if($user_set->fetchColumn()>0){
+        // Check if value matches anything in the database
+        $check_match_query = 'SELECT * FROM tbl_user WHERE user_name =:username';
+        $check_match_query .= ' AND user_pass="'.$password.'"';
+        $user_match = $pdo->prepare($check_match_query);
+        $user_match->execute(
+            array(
+                ':username'=>$username,
+                ':password'=>$password
+            )
+        );
+
+        while($founduser = $user_match->fetch(PDO::FETCH_ASSOC)){
+            $id = $founduser['user_id'];
+            $_SESSION['user_id'] = $id;
+            $_SESSION['user_name'] = $founduser['user_fname'];
+        }
+
+        //update the user table and set the user_ip column to be $ip
+
+        //hint:write a proper sql query to update
+        //check syntax above to see how to execute the query correctly
+        //this is the query that i can use
+        $update = 'UPDATE tbl_user SET user_ip=:ip WHERE user_id = :id';
+        $user_update = $pdo->prepare($update);
+        $user_update->execute(
+            array(
+                ':ip' =>$ip,
+                ':id' =>$id
+            )
+            ); 
+
+
+        if(isset($id)){
+            redirect_to('index.php');
+        }else{
+            return 'Your Password is incorrect';
+        }
+    }else{
+        return 'User does not exist';
+    }
+}
+
+function confirm_logged_in(){
+    if(!isset($_SESSION['user_id'])){
+        redirect_to('admin_login.php');
+    }
+}
+
+function logout() {
+    session_destroy();
+    redirect_to('admin_login.php');
+}
